664b141a650acbdde7b21e05962e22dbe49e8d20
SamiTlili <62347617+SamiTlili@users.noreply.github.com>
2023-11-21 21:47:22 +0100

    chore: init hello world api

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+    console.log('Server running init()');
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request: any, h: any) => {
+            return 'Hello World!';
+        }
+    });
+
+    await server.start();
+    console.info('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+    console.info(err);
+    process.exit(1);
+});
+
+init();
